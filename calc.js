504480c969d2168135fa30445ce062f86b577193
Wilber Claudio <wilberclaudio@gmail.com>
2023-02-06 17:07:36 -0500

    cleaned up code and gave better names to the arrays with characters

--- calc.js
+++ calc.js
@@ -1,36 +1,31 @@
 
 
-const btn_chars = ["C", "+/-", "%", "/", "7", "8","9", "x", 
+const btn_chars = ["C", "+/-", "%", "÷", "7", "8","9", "×", 
 "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="]
+const operator_btns = new Set(["÷", "×", "-", "+", "="]);
+const special_btns = new Set(["C", "+/-", "%"]);
+let operand = 0;
+let result = 0;
+const display_label = document.querySelector("label");
+display_label.appendChild(document.createTextNode(`${result}`));
 
-const orange_btns = new Set(["/", "x", "-", "+", "="]);
-const light_grey_btns = new Set(["C", "+/-", "%"]);
-function addBtns(){
+/**
+ * createBtnGrid generates the grid of buttons that will be on 
+ * the calculator. There will be 19 buttons in total.
+ * 
+ */
+function createBtnGrid(){
 
-    //define button_sections css properties to make a grid that
-    //is 5 x 4
     const btn_grid = document.getElementById(`btn-grid`);
 
-
-    //calculator will be 4 x 5, so 20 buttons
     for(c of btn_chars){
-        console.log(c);
-        //create button
         let btn = document.createElement('button');
-        //create content that will be in btn, For now I will put i
         let btn_content = document.createTextNode(`${c}`);
-
-        //determines what class to give current btn
-        if(orange_btns.has(c)){
-            btn.classList.add("operator");
-        }
-        else if(light_grey_btns.has(c)){
-            btn.classList.add("special");
-        } 
-
-        if(c =="0"){
-            btn.classList.add(`zero`);
-        }
+        btn.value = c;
+        //add style to button
+        addBtnClass(btn);
+        //add event listeners to each button
+        addBtnEventListener(btn, c);
         //add content to btn
         btn.appendChild(btn_content);
 
@@ -39,4 +34,51 @@ function addBtns(){
     }
 }
 
-addBtns();
+/**
+ * addBtnClass will assign the proper css styling class to the 
+ * button that is passed in as a parameter.
+ * 
+ */
+function addBtnClass(button){
+    //determines what class to give current btn
+    if(operator_btns.has(c)){
+        button.classList.add("operator");
+    }
+    else if(special_btns.has(c)){
+        button.classList.add("special");
+    } 
+    if(c =="0"){
+        button.classList.add(`zero`);
+    }
+}
+
+function addBtnEventListener(button, button_char){
+    //create cases for the kinds of buttons possible
+    
+    //button is decimal
+    if(button_char == "."){
+
+    }
+    //button is a special button
+    if(special_btns.has(button_char)){
+        //assign the proper function to this button depending on char
+    }
+    //button is operator
+    else if(operator_btns.has(button_char)){
+        //assign the proper function to this button depending on char
+    }
+    //button is a number
+    else{
+        button.addEventListener("click", (e) =>{
+            console.log(`${e.srcElement.value} was pressed`);
+        })
+    }
+
+}
+
+
+function updateResult(){
+    display_label.removeChild
+}
+
+createBtnGrid();
